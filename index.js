ae6f991a90864654ba4c3da3c26070f82f720572
Tayler <taylerblevins@gmail.com>
2024-02-05 13:35:46 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+  ];
+  
+  function superbowlWin(record) {
+    for (let i = 0; i < record.length; i++) {
+      const result = record[i];
+      if (result.result === "W") {
+        return result.year;
+      }
+    }
+}
